70024303b86bcdf4db86392472b1e2e22fe90424
Jesus Govanny Valdez C <jesus.vc@tecjerez.edu.mx>
2025-12-10 02:36:34 -0600

    Activities añadidas al manifest

--- app/src/main/java/com/example/proyecto_final_taller_bd_android/MenuModelosActivity.java
+++ app/src/main/java/com/example/proyecto_final_taller_bd_android/MenuModelosActivity.java
@@ -1,4 +1,14 @@
 package com.example.proyecto_final_taller_bd_android;
 
-public class MenuModelosActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuModelosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_modelos);
+    }
 }
+
